bc2b15fa8c9f6165b05a8f7bcc52d11d867738e2
Henrique Silva <henrique@dryworks.com.br>
2020-01-16 22:47:41 -0300

    feat: create user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users,
+             path: 'auth',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               password: 'secret',
+               confirmation: 'verification',
+               unlock: 'unblock',
+               registration: 'register',
+               sign_up: 'cmon_let_me_in'}
+
+  root to: 'movies#index'
+
+  get 'movies/index'
 end
