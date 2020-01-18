6890e2e279a520e2f8bae3ed64295a50ee50732c
Henrique Silva <henrique@dryworks.com.br>
2020-01-18 15:59:02 -0300

    test: add specs for action create

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,19 +2,62 @@ require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
   describe 'GET #index' do
-    let(:call_request) { get :index }
+    subject { get :index }
 
     context 'when user signed in' do
       login_user
-      before(:each) { call_request }
 
-      it { expect(response).to have_http_status(200) }
+      it { expect(subject).to have_http_status(200) }
     end
 
     context 'when user not signed in' do
-      before(:each) { call_request }
 
-      it { expect(response).to have_http_status(302) }
+      it { expect(subject).to have_http_status(302) }
+    end
+  end
+
+  describe 'POST #create' do
+    login_user
+    let(:current_user) { User.first }
+    let(:user_id) { current_user.id }
+    let(:user_movies) { current_user.movies }
+
+    subject { post :create,
+      params: {
+        use_route: user_movies_path(user_id),
+        movie: movie_params,
+        user_id: user_id
+      }
+    }
+
+    context 'with valid attributes' do
+      let(:movie_params) {{ name: Faker::Lorem.sentence, url: Faker::Internet.url }}
+
+      it 'should save the new movie in the database' do
+        subject
+        expect(user_movies.empty?).to be_falsey
+      end
+
+      it 'should redirect to the movies#show page' do
+        expect(subject).to render_template(:index)
+      end
+
+      it { expect(subject).to have_http_status(201) }
+    end
+
+    context 'with invalid attributes' do
+      let(:movie_params) {{ name: Faker::Lorem.sentence, url: '' }}
+
+      it 'should not save the new movie in the database' do
+        subject
+        expect(user_movies.empty?).to be_truthy
+      end
+
+      it 'should render movies#new template' do
+        expect(subject).to render_template(:new)
+      end
+
+      it { expect(subject).to have_http_status(200) }
     end
   end
 end
