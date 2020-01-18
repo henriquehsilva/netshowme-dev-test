172cf7419144207693d59611c624f1f50c58f662
Henrique Silva <henrique@dryworks.com.br>
2020-01-18 02:03:24 -0300

    feat: add actions create and new

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,24 @@
 class MoviesController < ApplicationController
   def index
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_index_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :url).merge( user_id: params[:user_id])
+  end
 end
