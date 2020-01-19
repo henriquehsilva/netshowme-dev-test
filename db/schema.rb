5615eba1927321f00256464bd886329123849237
Henrique Silva <henrique@dryworks.com.br>
2020-01-19 16:58:41 -0300

    feat: add views and poster to movies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_18_021319) do
+ActiveRecord::Schema.define(version: 2020_01_19_040630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_01_18_021319) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "views", default: 0
+    t.string "poster"
     t.index ["user_id"], name: "index_movies_on_user_id"
   end
 
